cc5c72012585055c21817821809b070154a8b4a7
heohl <65505245+heohl@users.noreply.github.com>
2021-02-10 10:40:12 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ tickerSymbol ='TSLA'
 tickerData=yf.Ticker(tickerSymbol)
 
 heute=date.today()
-tickerDf = tickerData.history(perdiod='1d', start = '2010-5-31', end = heute)
+tickerDf = tickerData.history(perdiod='1d', start = '2015-5-31', end = heute)
 
 st.line_chart(tickerDf.Close)
 st.line_chart(tickerDf.Volume)
